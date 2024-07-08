5f5f6a4cb91945a83d76f73377c4f2639a958ddc
omana <omanandswami2005@gmail.com>
2024-07-08 23:11:32 +0530

    vite config changed 1

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
+  build: {
+    outDir: '../server/public',
+
+    rollupOptions: {
+      input: {
+        main: '/src/main.jsx',
+        // login: './login.html',
+      },
+    },
+  },
   // proxy setup
   server: {
     proxy: {
