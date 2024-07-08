c768ce9befdbdbae9ce1e1d87077f449da9d7847
omana <omanandswami2005@gmail.com>
2024-07-08 23:22:36 +0530

    vite config changed 4

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   build: {
+    manifest: true,
     rollupOptions: {
-      input: {
-        main: '/src/main.jsx',
-        // login: './login.html',
-      },
+      input: '/src/main.jsx'
     },
   },
   // proxy setup
