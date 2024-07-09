cd365e2b905f60c516c2ff87dfe73364068cedf0
omana <omanandswami2005@gmail.com>
2024-07-09 09:11:50 +0530

    Vite config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      // This ensures that all the routes fallback to index.html
-      input: {
-        // eslint-disable-next-line no-undef
-        main: path.resolve(__dirname, 'index.html'),
-      }
-    }
+    outDir: 'dist'
   },
   server: {
     proxy: {
