d89cc07c2909cd20a1db667852bcccffc5d455b1
omana <omanandswami2005@gmail.com>
2024-07-09 07:09:19 +0530

    Vite config changed 5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   build: {
-    manifest: true,
     rollupOptions: {
-      input: '/src/main.jsx'
-    },
+      input: {
+        // eslint-disable-next-line no-undef
+        main: resolve(__dirname, 'index.html'),
+    },  
   },
-  // proxy setup
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://stabsbackend.onrender.com',
-        changeOrigin: true,
-      }
-    }
-  }
-
+  },
 })
